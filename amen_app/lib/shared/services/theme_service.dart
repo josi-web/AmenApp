5f5175250ede386d33a30a433d1e1ae11370342d
Yohannes <yohannesdawit360@gmail.com>
2025-04-12 22:12:09 +0300

    Remove unused import from ThemeService: Clean up code by eliminating unnecessary dependencies to improve maintainability.

--- amen_app/lib/shared/services/theme_service.dart
+++ amen_app/lib/shared/services/theme_service.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/rendering.dart';
 
 enum ThemeType { classic, day, tinted, night }
 
